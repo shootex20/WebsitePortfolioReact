49f9b7d98f4fba5c33485fc528c6709d5067d55e
Chelsey Coughlin <chelsey.coughlin@sparrowconnected.com>
2021-11-21 01:53:23 -0700

    Much more changes to make the ui slightly sleeker.

--- src/componets/NavbarImport.jsx
+++ src/componets/NavbarImport.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link, animateScroll as scroll } from "react-scroll";
 
 
 export const NavbarImport = () => {
-    return (
-      <>
-      <div className="navBarIn">
-  <Navbar fixed="top" className="justify-content-center navbar fixed-top navbar-expand-sm navbarColor" variant="dark">
-  <Navbar.Brand><Link smooth={true} to="home">Home</Link> </Navbar.Brand>
-  <Navbar.Brand><Link smooth={true} to="aboutme">About me</Link></Navbar.Brand>
-  <Navbar.Brand><Link smooth={true} to="projects">Project</Link></Navbar.Brand>
-  <Navbar.Brand><Link smooth={true} to="resume">Resume</Link></Navbar.Brand>
-  </Navbar>
-  </div>
-  </>
-    )
+  return (
+    <>
+      <div >
+
+        <Nav fill variant="tabs" defaultActiveKey="home" fixed="top" className="justify-content-center navbar fixed-top navbar-expand-sm navbarColor ">
+          <Nav.Item>
+            <Link smooth={true} to="home"><Nav.Link eventKey="home">Home</Nav.Link> </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link smooth={true} to="aboutme"><Nav.Link eventKey="aboutme">About me</Nav.Link></Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link smooth={true} to="projects"><Nav.Link eventKey="projects">Projects</Nav.Link> </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link smooth={true} to="resume"> <Nav.Link eventKey="resume">Resume</Nav.Link></Link>
+          </Nav.Item>
+        </Nav>
+
+      </div>
+    </>
+  )
 }
 
 export default NavbarImport
--- src/componets/aboutme.jsx
+++ src/componets/aboutme.jsx
@@ -3,32 +3,23 @@ import photos from './images/profile.jpg';
 
 export const aboutme = () => {
     return (
-
         <div className="container-fluid justify-content-md-center" id="aboutme">
-            
-        <div className="shrinkBoxes">
-            <div className="justify-content-md-center">
-
-                <div className="card justify-content-md-center">
-
-            <img src={photos} className="img-fluid centerImage2" width={280} height={350}/>
-
-        <div className="textCenter">
-        <div className="shrinkBoxes">
-            <h5>About me</h5>
-                                    <p>Graduated from Southern Alberta Institute of Technology majoring in Software Development. GPA of 3.56, and strong problem solving skills.</p>
-                                    <p>I’m consistently looking to expand my knowledge as a software developer by learning new or interesting programming languages. I am very fluent with Algorithms and data structures.</p>
-                                    <p>Current Languages: Java, SQL (Oracle / MySQL), C#, Ruby, HTML5, CSS, JSP's</p>
-                                    <p>Familiar Languages: Javascript, React.js</p>
-                                    <p>Current Frameworks: Maven, Git, Ubuntu, Tomcat. Some experience with AWS, Google Cloud, Heroku.</p>
-                                    <p>Currently learning: Node.js, MongoDB,  Express, GraphSQL.</p>
-
-                                    </div>
-                                    </div>
-                                    </div>
-
+            <div className="shrinkBoxes">
+                <div className="justify-content-md-center">
+                    <div className="card justify-content-md-center">
+                        <img src={photos} className="img-fluid centerImage2" width={280} height={350} />
+                        <div className="textCenter">
+                            <div className="shrinkBoxes">
+                                <h5>About me</h5>
+                                <p>Graduated from Southern Alberta Institute of Technology majoring in Software Development. GPA of 3.56, and strong problem solving skills.</p>
+                                <p>I’m consistently looking to expand my knowledge as a software developer by learning new or interesting programming languages. I am very fluent with Algorithms and data structures and a strong ability to solve problems.</p>
+                                <p>Current Languages: Java, SQL (Oracle / MySQL), C#, Ruby, HTML5, CSS, JSP's, Javascript, React.js, TypeScript</p>
+                                <p>Current Frameworks/Clouds: Maven, Git, Ubuntu, Tomcat. Some experience with AWS, Google Cloud, Heroku, Azure.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
